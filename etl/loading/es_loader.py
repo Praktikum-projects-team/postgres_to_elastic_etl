0350a6cf99c396f688f09e9fd20292c3c7b93240
Liana <liana.nigmatullina@tetrika.school>
2023-04-15 12:53:21 +0300

    refactor

--- etl/loading/es_loader.py
+++ etl/loading/es_loader.py
@@ -11,14 +11,17 @@ class ElasticsearchLoader:
         self.index_name = 'movies'
         self.es = Elasticsearch(es_host, verify_certs=False)
 
-    def create_index_if_not_exists(self):
-        if not self.es.indices.exists(index=self.index_name):
-            self.es.indices.create(index=self.index_name, mappings=movies_mappings, settings=movies_settings)
+    def _create_index_if_not_exists(self):
+        if self.es.indices.exists(index=self.index_name):
+            return
+        logging.info('index does not exist, creating...')
+        self.es.indices.create(index=self.index_name, mappings=movies_mappings, settings=movies_settings)
+        logging.info('index created')
 
     @backoff.on_exception(backoff.expo, ConnectionError, max_time=300)
     def load_data(self, data: dict):
+        self._create_index_if_not_exists()
         actions = [{'_index': self.index_name, '_id': doc['id'], '_source': doc} for doc in data]
         rows_count, errors = helpers.bulk(self.es, actions=actions)
         if errors:
             logging.error('loading data error:', extra={'actions': actions, 'errors': errors})
-
